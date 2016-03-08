4fcf051709cca6795226c92d5960d2adbe32074d
Paul Beck <pb@teamgeist-medien.de>
2016-03-08 09:53:38 +0100

    Solves #8 - Missing copyright field in TYPO3 6.2

---
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -0,0 +1,17 @@
+<?php
+
+$temp_metacolumns = array(
+	'copyright' => array(
+		'exclude' => 1,
+		'label' => 'Copyright',
+		'config' => array(
+			'type' => 'input',
+			'size' => 20,
+			'eval' => 'trim'
+		),
+	),
+);
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_metadata',$temp_metacolumns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('sys_file_metadata','','copyright','after:source');
--- ext_tables.sql
+++ ext_tables.sql
@@ -6,3 +6,12 @@ CREATE TABLE sys_file_reference (
 	copyright varchar(255) DEFAULT NULL,
 
 );
+
+#
+# Table structure for table 'sys_file_metadata'
+#
+CREATE TABLE sys_file_metadata (
+
+	copyright varchar(255) DEFAULT '' NOT NULL,
+
+);
