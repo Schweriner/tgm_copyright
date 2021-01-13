ec74bc498b7329e52e1c9972144ae0f222cade6c
Olli <oliver.pfaff.email@gmail.com>
2021-01-13 13:22:24 +0100

    #33
    in TYPO3 10 this field already exists, so we can remove this to avoid errors

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,10 +4,3 @@
 CREATE TABLE sys_file_reference (
 	copyright varchar(255) DEFAULT NULL
 );
-
-#
-# Table structure for table 'sys_file_metadata'
-#
-CREATE TABLE sys_file_metadata (
-	copyright varchar(255) DEFAULT '' NOT NULL
-);
