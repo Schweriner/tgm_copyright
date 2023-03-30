107818136f55ffe468494af90ebfde6e544ed051
Paul Beck <p.beck@nerdost.net>
2023-03-30 14:10:22 +0200

    Raised version

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
   'state' => 'beta',
   'uploadfolder' => false,
   'clearCacheOnLoad' => 1,
-  'version' => '3.2.1',
+  'version' => '3.2.2',
   'constraints' => 
   [
     'depends' => 
