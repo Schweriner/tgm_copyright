a1d2a546087d135a3b07190c8fc6adbf38a2940d
Paul Beck <hi@toll-paul.de>
2023-12-08 12:42:28 +0100

    [FIX] Dont add JS to frontend when logged into backend

--- ext_tables.php
+++ ext_tables.php
@@ -6,7 +6,7 @@ if (!defined('TYPO3')) {
 (function() {
     /** @var \TYPO3\CMS\Core\Context\Context $context */
     $context = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Context\Context::class);
-    if(true === (bool) $GLOBALS['TYPO3_CONF_VARS'['EXTENSIONS']['tgm_copyright']]['copyrightRequired']
+    if(true === (bool) $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tgm_copyright']]['copyrightRequired']
         && true === \TYPO3\CMS\Core\Http\ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()
     )
      {
