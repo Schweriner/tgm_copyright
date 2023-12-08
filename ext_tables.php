2ac0c15e5b7e029abd702b3fd4347638fa5bc985
Paul Beck <hi@toll-paul.de>
2023-12-08 12:41:47 +0100

    [FIX] Dont add JS to frontend when logged into backend

--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,12 @@ if (!defined('TYPO3')) {
 }
 
 (function() {
-    if(true === (bool) \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)
-            ->get('tgm_copyright', 'copyrightRequired')) {
+    /** @var \TYPO3\CMS\Core\Context\Context $context */
+    $context = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Context\Context::class);
+    if(true === (bool) $GLOBALS['TYPO3_CONF_VARS'['EXTENSIONS']['tgm_copyright']]['copyrightRequired']
+        && true === \TYPO3\CMS\Core\Http\ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()
+    )
+     {
         try {
             $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
             $pageRenderer->loadRequireJsModule('TYPO3/CMS/TgmCopyright/RequiredFileReferenceFields');
